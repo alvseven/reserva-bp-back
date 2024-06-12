bd48552d1aa9f89be4f7ac52572f9e97faa06bde
alvseven <alv7contato@outlook.com>
2024-06-12 09:24:54 -0300

    feat: create get user logged in endpoint for customers and insurance brokers

--- src/modules/customers/routes/customers.ts
+++ src/modules/customers/routes/customers.ts
@@ -17,102 +17,122 @@ import { createSchedulingUseCase } from "../use-cases/create-scheduling.js";
 import { deleteCustomerUseCase } from "../use-cases/delete-customer.js";
 import { getCustomerUseCase } from "../use-cases/get-customer.js";
 import { loginUseCase } from "../use-cases/login.js";
+import { getCustomerLoggedInUseCase } from "../use-cases/get-customer-logged-in.js";
 import { updateCustomerUseCase } from "../use-cases/update-customer.js";
+import verifyAuth from "@/shared/middlewares/verify-auth.js";
 
 export async function customersRoutes(fastify: FastifyInstance) {
-	const customersRepository = new MongooseCustomersRepository();
-	const insuranceBrokersRepository = new MongooseInsuranceBrokersRepository();
+  const customersRepository = new MongooseCustomersRepository();
+  const insuranceBrokersRepository = new MongooseInsuranceBrokersRepository();
 
-	fastify.withTypeProvider<ZodTypeProvider>().post(
-		"/",
-		{
-			schema: {
-				body: createCustomerRequestSchema,
-			},
-		},
-		async (req, reply) => {
-			const createdCustomer = await createCustomerUseCase(
-				req.body,
-				customersRepository,
-			);
+  fastify.withTypeProvider<ZodTypeProvider>().post(
+    "/",
+    {
+      schema: {
+        body: createCustomerRequestSchema,
+      },
+    },
+    async (req, reply) => {
+      const createdCustomer = await createCustomerUseCase(
+        req.body,
+        customersRepository
+      );
 
-			return reply.status(201).send(createdCustomer);
-		},
-	);
+      return reply.status(201).send(createdCustomer);
+    }
+  );
 
-	fastify.withTypeProvider<ZodTypeProvider>().post(
-		"/login",
-		{
-			schema: {
-				body: loginRequestSchema,
-				response: {
-					"200": loginResponseSchema,
-				},
-			},
-		},
-		async (req, reply) => {
-			const customerLogged = await loginUseCase(req.body, customersRepository);
+  fastify.withTypeProvider<ZodTypeProvider>().post(
+    "/login",
+    {
+      schema: {
+        body: loginRequestSchema,
+        response: {
+          "200": loginResponseSchema,
+        },
+      },
+    },
+    async (req, reply) => {
+      const customerLoggedIn = await loginUseCase(
+        req.body,
+        customersRepository
+      );
 
-			return reply.send(customerLogged);
-		},
-	);
+      return reply.send(customerLoggedIn);
+    }
+  );
 
-	fastify.withTypeProvider<ZodTypeProvider>().post(
-		"/me/schedulings",
-		{
-			schema: {
-				body: createSchedulingRequestSchema,
-			},
-		},
-		async (req, reply) => {
-			const createdScheduling = await createSchedulingUseCase(req.body, {
-				customersRepository,
-				insuranceBrokersRepository,
-			});
+  fastify.withTypeProvider<ZodTypeProvider>().post(
+    "/me",
+    {
+      preHandler: verifyAuth,
+    },
+    async (req, reply) => {
+      const customerLoggedIn = await getCustomerLoggedInUseCase(
+        req.user.id,
+        customersRepository
+      );
 
-			return reply.status(201).send(createdScheduling);
-		},
-	);
+      return reply.send(customerLoggedIn);
+    }
+  );
 
-	fastify.withTypeProvider<ZodTypeProvider>().get(
-		"/:id",
-		{
-			schema: {
-				params: getCustomerRequestSchema,
-			},
-		},
-		async (req, reply) => {
-			const user = await getCustomerUseCase(req.params.id, customersRepository);
+  fastify.withTypeProvider<ZodTypeProvider>().post(
+    "/me/schedulings",
+    {
+      schema: {
+        body: createSchedulingRequestSchema,
+      },
+    },
+    async (req, reply) => {
+      const createdScheduling = await createSchedulingUseCase(req.body, {
+        customersRepository,
+        insuranceBrokersRepository,
+      });
 
-			return reply.send(user);
-		},
-	);
+      return reply.status(201).send(createdScheduling);
+    }
+  );
 
-	fastify.withTypeProvider<ZodTypeProvider>().patch(
-		"/:id",
-		{
-			schema: {
-				body: updateCustomerRequestSchema,
-			},
-		},
-		async (req, reply) => {
-			await updateCustomerUseCase(req.body, customersRepository);
+  fastify.withTypeProvider<ZodTypeProvider>().get(
+    "/:id",
+    {
+      schema: {
+        params: getCustomerRequestSchema,
+      },
+    },
+    async (req, reply) => {
+      const user = await getCustomerUseCase(req.params.id, customersRepository);
 
-			return reply.status(204).send();
-		},
-	);
+      return reply.send(user);
+    }
+  );
 
-	fastify.withTypeProvider<ZodTypeProvider>().delete(
-		"/:id",
-		{
-			schema: {
-				params: deleteCustomerRequestSchema,
-			},
-		},
-		async (req, reply) => {
-			await deleteCustomerUseCase(req.params.id, customersRepository);
+  fastify.withTypeProvider<ZodTypeProvider>().patch(
+    "/:id",
+    {
+      schema: {
+        body: updateCustomerRequestSchema,
+      },
+    },
+    async (req, reply) => {
+      await updateCustomerUseCase(req.body, customersRepository);
 
-			return reply.status(204).send();
-		},
-	);
+      return reply.status(204).send();
+    }
+  );
+
+  fastify.withTypeProvider<ZodTypeProvider>().delete(
+    "/:id",
+    {
+      schema: {
+        params: deleteCustomerRequestSchema,
+      },
+    },
+    async (req, reply) => {
+      await deleteCustomerUseCase(req.params.id, customersRepository);
+
+      return reply.status(204).send();
+    }
+  );
 }
---
+++ src/modules/customers/use-cases/get-customer-logged-in.ts
@@ -0,0 +1,16 @@
+import { httpErrors } from "@fastify/sensible";
+
+import type { CustomersRepository } from "../contracts/customer.js";
+
+export async function getCustomerLoggedInUseCase(
+  id: string,
+  customersRepository: CustomersRepository
+) {
+  const customerFound = await customersRepository.findById(id);
+
+  if (!customerFound) {
+    throw httpErrors.notFound("Cliente não encontrado");
+  }
+
+  return customerFound;
+}
